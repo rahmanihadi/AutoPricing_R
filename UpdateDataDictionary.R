9a230faaaa91dad7335266000e0019a233f53c11
rahmanihadi <71031643+rahmanihadi@users.noreply.github.com>
2020-11-17 12:48:52 +0000

    adds log-print to UpdateDataDictionary.R, atomic commit

--- UpdateDataDictionary.R
+++ UpdateDataDictionary.R
@@ -12,19 +12,53 @@ UpdateDataDictionary <- function(Brand, Product, Transaction, Source, data_dicti
   DD0 <- xx1[grepl(Transaction, xx1[["Transaction"]]) & grepl(Product, xx1[["Product"]]) & grepl(Source, xx1[["Source"]]), ]
   
   # rename those Status=Rename but Type not equal to Review
+  
+  PrintComment(capture_log$prefix, 4, 2, paste0('Renaming `Field`s satisfying : ', '`Status` == "Rename" & `Type` != "Review"', ' to `Live_Name`'))
+ 
+  DD0_keep <- DD0
+  
   mask <- (DD0$Status == "Rename") & (DD0$Type != "Review")
   DD0[mask, "Field"] <- DD0[mask, "Live_Name"]
   
+  DD0_keep_p <- DD0_keep[mask, ]
+  DD0_p <- DD0[mask, ]
+  
+  for(i in 1:nrow(DD0_p)){
+    
+    PrintComment(capture_log$prefix, 5, 2, paste0(c(DD0_keep_p[i, "Field"], DD0_p[i, "Field"]), collapse = ' --> '))
+    
+  }
+  
   # Those of Type = "Review" are renamed to "Review_" + "Live_Name"
-
+  
+  PrintComment(capture_log$prefix, 4, 2, paste0('Renaming `Field`s satisfying :' , '`Type` == "Review"',' to Review_`Live_Name`'))
+  
+  DD0_keep <- DD0
+  
   mask = DD0$Type == "Review"
-  if(sum(mask) !=0){DD0[mask, "Field"] <-  paste0("Review_", DD0[mask, "Live_Name"])} 
+  DD0_keep_p <- DD0_keep[mask, ]
+  DD0_p <- DD0[mask, ]
+  
+  if(sum(mask) !=0){
+    
+    DD0[mask, "Field"] <-  paste0("Review_", DD0[mask, "Live_Name"])
+    
+  } 
+  
+  for(i in 1:nrow(DD0_p)){
+    
+    PrintComment(capture_log$prefix, 5, 2, paste0(c(DD0_keep_p[i, "Field"], DD0_p[i, "Field"]), collapse = ' --> '))
+    
+  }
   
   # selecting only seven columns 
+  
   NotYetThere <- DD0[, c("Field", "Live_Name", "Earnix_Mapping", "Type", "Format", "Missing_Value", "Submodel")]
   
   # dropping the duplicated rows 
+  
   UpdateDictionary <- unique(NotYetThere)
 
   return(UpdateDictionary)
+  
 }
